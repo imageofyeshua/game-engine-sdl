401c678a5b751a266816b2c1e6e2b591403c6034
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-02 20:14:40 +0900

    Draw Paddle and Ball

--- GameDesignSDL/Game.cpp
+++ GameDesignSDL/Game.cpp
@@ -8,6 +8,9 @@
 
 #include "Game.h"
 
+const int thickness = 15;
+const float paddleH = 100.0f;
+
 Game::Game()
 	:mWindow(nullptr)
 	, mIsRunning(true)
@@ -52,6 +55,13 @@ bool Game::Initialize()
 		SDL_Log("Failed to create renderer: %s", SDL_GetError());
 		return false;
 	}
+
+	mPaddlePos.x = 10.0f;
+	mPaddlePos.y = 768.0f / 2.0f;
+	mBallPos.x = 1024.0f / 2.0f;
+	mBallPos.y = 768.0f / 2.0f;
+	mBallVel.x = -200.0f;
+	mBallVel.y = 235.0f;
 
 	return true;
 }
@@ -101,12 +111,57 @@ void Game::GenerateOutput()
 		mRenderer,
 		0,		// R
 		0,		// G 
-		255,	// B
+		0,	// B
 		255		// A
 	);
 
 	// Clear back buffer
 	SDL_RenderClear(mRenderer);
+
+	// Draw walls
+	SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
+
+	// Draw top wall
+	SDL_Rect wall
+	{
+		0,
+		0,
+		1024,
+		thickness
+	};
+
+	SDL_RenderFillRect(mRenderer, &wall);
+
+	// Draw bottom wall
+	wall.y = 768 - thickness;
+	SDL_RenderFillRect(mRenderer, &wall);
+
+	// Draw right wall
+	wall.x = 1024 - thickness;
+	wall.y = 0;
+	wall.w = thickness;
+	wall.h = 1024;
+	SDL_RenderFillRect(mRenderer, &wall);
+
+	// Draw paddle
+	SDL_Rect paddle{
+		static_cast<int>(mPaddlePos.x),
+		static_cast<int>(mPaddlePos.y - paddleH / 2),
+		thickness,
+		static_cast<int>(paddleH)
+	};
+
+	SDL_RenderFillRect(mRenderer, &paddle);
+
+	// Draw ball
+	SDL_Rect ball{
+		static_cast<int>(mBallPos.x - thickness / 2),
+		static_cast<int>(mBallPos.y - thickness / 2),
+		thickness,
+		thickness
+	};
+
+	SDL_RenderFillRect(mRenderer, &ball);
 
 	// Swap front buffer and back buffer
 	SDL_RenderPresent(mRenderer);
