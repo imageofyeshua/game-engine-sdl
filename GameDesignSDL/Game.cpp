16118730536e3b17caf7781dfb62f2d624324d25
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-02 19:20:33 +0900

    Initial Commit

---
+++ GameDesignSDL/Game.cpp
@@ -0,0 +1,104 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#include "Game.h"
+
+Game::Game()
+	:mWindow(nullptr)
+	, mIsRunning(true)
+{
+
+}
+
+bool Game::Initialize()
+{
+	// Initialize SDL
+	int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+	if (sdlResult != 0)
+	{
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return false;
+	}
+
+	// Create an SDL Window
+	mWindow = SDL_CreateWindow(
+		"Game Engine", // Window title
+		100,	// Top left x-coordinate of window
+		100,	// Top left y-coordinate of window
+		1024,	// Width of window
+		768,	// Height of window
+		0		// Flags (0 for no flags set)
+	);
+
+	if (!mWindow)
+	{
+		SDL_Log("Failed to create window: %s", SDL_GetError());
+		return false;
+	}
+
+	return true;
+}
+
+void Game::RunLoop()
+{
+	while (mIsRunning)
+	{
+		ProcessInput();
+		UpdateGame();
+		GenerateOutput();
+	}
+}
+
+void Game::ProcessInput()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+			// If we get an SDL_QUIT event, end loop
+		case SDL_QUIT:
+			mIsRunning = false;
+			break;
+		}
+	}
+
+	// Get state of keyboard
+	const Uint8* state = SDL_GetKeyboardState(NULL);
+	// If escape is pressed, also end loop
+	if (state[SDL_SCANCODE_ESCAPE])
+	{
+		mIsRunning = false;
+	}
+
+	// Update paddle direction based on W/S keys
+	mPaddleDir = 0;
+	if (state[SDL_SCANCODE_W])
+	{
+		mPaddleDir -= 1;
+	}
+	if (state[SDL_SCANCODE_S])
+	{
+		mPaddleDir += 1;
+	}
+}
+
+void Game::UpdateGame()
+{
+}
+
+void Game::GenerateOutput()
+{
+}
+
+void Game::Shutdown()
+{
+	SDL_DestroyWindow(mWindow);
+	SDL_Quit();
+}
+
